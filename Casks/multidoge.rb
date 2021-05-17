8a79a99e5ef2b83d50df5ee834ae43d1c10aeac5
hjxfire <hjxfire@gmail.com>
2021-05-17 13:42:41 +0800

    update multidoge.rb

--- Casks/multidoge.rb
+++ Casks/multidoge.rb
@@ -1,6 +1,6 @@
 cask 'multidoge' do
     version '0.1.7'
-    sha256 '648751ce2598d838b07b65dd5ff3e4136b0c369ddc38efedd9329a63365506f5'
+    sha256 '2785d79cce18d631dd8650e9eaa7fe10c084e4b65cc30c49c5a9d74e94ffcaf6'
   
     # github.com is the official download host per the vendor homepage
     url "https://github.com/langerhans/multidoge/releases/download/v#{version}/multidoge-#{version}.dmg"
